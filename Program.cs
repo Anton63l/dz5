e6a9811bf7f21789356c4d518159baf3549b9bc8
Anton <anton@mail.ru>
2022-09-19 21:38:50 +0400

    Решена вторая задача

--- Program.cs
+++ Program.cs
@@ -28,3 +28,39 @@
 
 // var array = GenerateArray(10);
 // PrintArray(array);
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+
+// int[] GenerateArray(int length) {
+//     var array = new int[length];
+
+//     var random = new Random();
+
+//     for (var i = 0; i < array.Length; i++) {
+//         array[i] = random.Next(-100, 101);
+//     }
+
+//     return array;
+// }
+
+// void PrintArray(int[] array) {
+//     int sum = 0;
+
+//     // Выводим на печать массив
+//     for (var i = 0; i < array.Length-1; i++) {
+//         Console.Write(array[i] + ", ");
+        
+//     }
+    
+//     Console.WriteLine(array[array.Length - 1]);
+
+// // Выводим на печать сумму четных элементов
+//     for (var i = 1; i < array.Length; i+=2) {
+//             sum += array[i];
+//     }
+
+//     System.Console.WriteLine($"Сумма нечетных элементов массива равна: {sum}");
+// }
+
+// var array = GenerateArray(10);
+// PrintArray(array);
